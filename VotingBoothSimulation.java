b105320bb7228b490a6da677b5024dd28dadf616
JackCottrell <cottrelj@mail.gvsu.edu>
2016-06-27 22:40:08 +0000

    Add files via upload
    BAM! overall GUI class

---
+++ VotingBoothSimulation.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class VotingBoothSimulation{
+
+	public static void main (String[] args)
+	{	
+		JFrame frame = new JFrame ("Voting Booth Simulation");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		VotingBoothPanel panel = new VotingBoothPanel();
+		frame.getContentPane().add(panel);
+		frame.setSize(600, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
