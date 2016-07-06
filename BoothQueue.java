2462862f35311a7a7da473440a8111862ac4569d
JackCottrell <cottrelj@mail.gvsu.edu>
2016-07-06 17:53:54 -0400

    Add files via upload

--- BoothQueue.java
+++ BoothQueue.java
@@ -1,3 +1,5 @@
+package VoterSim;
+
 import java.util.ArrayList;
 
 public class BoothQueue implements ClockListener {
@@ -10,44 +12,40 @@ public class BoothQueue implements ClockListener {
 	private Voter person;   
 
 	//increments everytime someone leaves the q
-	private int completed = 0;
-
-	//arraylist of booths
-	private Booth[] booths;
-
-	//number of booths in arraylist
-	private int numBooths; 
-
-	//constructor and stuff
-	public BoothQueue(Booth[] booths){
-		this.booths = booths;
-		numBooths = booths.length;
-	}
+	private int deserters = 0;
+	
+//	//Overall Stats Class
+//	private SimulationState stats;
+//	
+//	//Brings in SimulationState in 
+//	public BoothQueue(SimulationState stats){
+//		this.stats = stats;
+//	}
 
 	//adds person to Q
-	public void add (Voter person)
+	public void add(Voter person)
 	{
 		Q.add(person);
 		if (Q.size() > maxQlength)
 			maxQlength = Q.size();
 	}
 
-	//check each booth if it is free
-	public void event (int tick){
-		if(!Q.isEmpty()){
-			for(int i = 0; i < numBooths; i++){
-				//if free add the person a front of line to that booth
-				//also remove them from Q
-				if(booths[i].isOpen()){
-					//record the time that the person is added to booth
-					Q.get(0).setBoothInTime(tick);
-					//remove from Q and add to booth
-					booths[i].add(Q.remove(0));
-				}
+	//remove and return the first person in line
+	public Voter getVoter(){
+		return Q.remove(0);
+	}
+	
+	//make sure no one wants to leave
+	public void event(int tick){
+		for(int i = 0; i < Q.size(); i++){
+			if(tick >= Q.get(i).getLeaveTime()){
+				Q.remove(i);
+				//increment people that left
 			}
 		}
 	}
 
+	//return number of voter in q
 	public int getLeft() {
 		return Q.size();
 	}
@@ -56,7 +54,8 @@ public class BoothQueue implements ClockListener {
 		return maxQlength;
 	}
 
-	public int getThroughPut() {
-		return completed;
+	//return how many people left the q
+	public int getDeserters() {
+		return deserters;
 	}
 }
--- Voter.java
+++ Voter.java
@@ -1,15 +1,31 @@
+package VoterSim;
 /**
  * @author   Roger Ferguson
  */
 public class Voter {
+	//total time 
 	private int tickTime;
+	
 	//the value of tick when the person enters a booth
 	private int boothInTime;
 	
 	// max time person stays in line
 	protected double boothTime;
+	
+	//time to check in
 	protected double checkInTime;
 	
+	//time untill voter leaves
+	protected double leaveTime;
+	
+	public double getLeaveTime() {
+		return leaveTime;
+	}
+
+	public void setLeaveTime(double leaveTime) {
+		this.leaveTime = leaveTime;
+	}
+
 	public double getBoothTime() {
 		return boothTime;
 	}
